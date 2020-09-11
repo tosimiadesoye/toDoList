39114d789f39a33cfe379cf431ce18e436b1c1d9
tosimiadesoye <tosimiadesoye111@gmail.com>
2020-09-11 13:19:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+let input = document.getElementById("input-task")
+let addBtn = document.getElementById("add-btn")
+let ol = document.querySelector("#ol-tag")
+
+
+addBtn.addEventListener("click", listItems)
+
+function listItems(){
+//delete btn
+let deleteBtn = document.createElement("span")
+    deleteBtn.setAttribute("class", "delete")
+let i = document.createElement("i")
+    i.setAttribute("class", "far fa-trash-alt")
+    deleteBtn.appendChild(i)
+//paragraph text
+let text = document.createElement("p")
+    text.setAttribute("class", "paragraph")
+    text.textContent = input.value
+//edit btn
+let editBtn = document.createElement("span")
+    editBtn.setAttribute("class", "edit")
+let secI = document.createElement("i")
+    secI.setAttribute("class", "far fa-edit")
+    editBtn.appendChild(secI)
+//check btn
+let checkBtn = document.createElement("span")
+    checkBtn.setAttribute("class", "check")
+let thirdI = document.createElement("i")
+        thirdI.setAttribute("class", "fas fa-check")
+        checkBtn.appendChild(thirdI)
+//list
+let li = document.createElement("li")
+    li.setAttribute("class", "list-li")
+//appending of multiple elements to list
+let appendElem = [checkBtn, text, deleteBtn, editBtn]
+    appendElem.forEach(elements => li.appendChild(elements))    
+//appending of list to ol
+ol.appendChild(li)
+
+//erasing the text on the input tab 
+input.value = "";
+}
+
+
+/*editBtn.onclick = editItem => {
+  let del = [].splice(0, -1)
+  del.style.display = ("none")
+}
+
+tick.onclick = tickedItem = () => {
+    paraItems.style.textDecorationLine = "line-through" 
+}*/
+
